f787ce0a4d8d806303d3ffe75498eadb2d920bc8
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-05-05 11:38:07 +0200

    Corrigé

--- Langage/ex-jeu.js
+++ Langage/ex-jeu.js
@@ -6,14 +6,49 @@ const rl = readline.createInterface({
 });
 
 // Enoncé slide 68
+function genererAlea(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-rl.question('Quel est le nombre ? ', (saisie) => {
-  // saisie est type string (à vous de convertir)
-  console.log('Vous avez saisi ' + saisie);
+function dejaJoues(essais) {
+  // Optional Chaining (ES2020)
+  // if (essais?.length) {
+  if (!(essais && essais.length)) {
+    return;
+  }
 
-  // rejouer (boucler en asynchrone)
+  console.log('Vous avez déjà joué : ' + essais.join(' | '));
+}
 
-  // arreter le programme
-  rl.close();
-});
+const entierAlea = genererAlea(0, 100);
+const essais = [];
 
+function loop() {
+  dejaJoues(essais);
+  rl.question('Quel est le nombre ? ', (saisie) => {
+    const entierSaisi = parseInt(saisie);
+
+    if (isNaN(entierSaisi)) {
+      console.error('Erreur : Il faut saisir un nombre');
+      return loop();
+    }
+
+    console.log('Vous avez saisi ' + entierSaisi);
+
+    essais.push(entierSaisi)
+  
+    if (entierSaisi < entierAlea) {
+      console.log('Trop petit');
+      loop();
+    } else if (entierSaisi > entierAlea) {
+      console.log('Trop grand');
+      loop();
+    } else {
+      console.log('Gagné');
+      rl.close();
+    }
+  });
+}
+loop();
