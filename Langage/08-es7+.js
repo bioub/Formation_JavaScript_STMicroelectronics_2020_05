3825bfae11aa8238fc437673c916558b6ae8c753
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2020-05-07 10:31:02 +0200

    Line

---
+++ Langage/08-es7+.js
@@ -0,0 +1,13 @@
+// ES7 / ES2016
+
+// Exponentiation operator
+console.log(2 ** 3); // 8
+
+// Includes tableaux
+console.log(['Romain', 'Jean'].includes('Jean')); // true
+console.log(['Romain', 'Jean'].includes('Eric')); // false
+
+// ES8 / ES2017
+const contact = {
+  firstName: 'Romain',
+};
